bd714feddf4e1cdc9e01ac7c76ef66ccf64f350a
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2020-11-06 14:38:05 +0000

    [PS|SS] TAV-141 - Initial lambda structure compatible with step functions

--- src/main/scala/AddressLookupFileDownloadFunction.scala
+++ src/main/scala/AddressLookupFileDownloadFunction.scala
@@ -1,19 +1,19 @@
 import java.util
 
-import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.jessecoyle.JCredStash
 
-class AddressLookupFileDownloadFunction {
-  def handler(input: String, context: Context): Int = {
+class AddressLookupFileDownloadFunction extends RequestHandler[String,  util.Map[String, util.List[String]]] with scala.collection.convert.AsJavaConverters {
+  override def handleRequest(input: String, context: Context): util.Map[String,  util.List[String]] = {
 //    val bucket = System.getenv("BUCKET_NAME") //Do we need this as files should be downloaded to EFS?
 //    val region = System.getenv("AWS_REGION") //Ditto - no need to reference this anywhere?
-    val user = AddressLookupFileDownloadFunction.retrieveCredential("address_lookup_file_download_user")
-    val password = AddressLookupFileDownloadFunction.retrieveCredential("address_lookup_file_download_password")
+//    val user = AddressLookupFileDownloadFunction.retrieveCredential("address_lookup_file_download_user")
+//    val password = AddressLookupFileDownloadFunction.retrieveCredential("address_lookup_file_download_password")
 
     //TODO: Actually download the file
 
-    0
+    mapAsJavaMap(Map("files" -> seqAsJavaList(Seq("file_1", "file_ 2", "file_3"))))
   }
 }
 
