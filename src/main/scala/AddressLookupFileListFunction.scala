ed64f299a612bb65cab18af28707c474c3fe03f5
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2020-11-18 16:45:50 +0000

    [PS|SS] TAV-141 - Invert map of products returned  from file list lambda so epoch is at the root

--- src/main/scala/AddressLookupFileListFunction.scala
+++ src/main/scala/AddressLookupFileListFunction.scala
@@ -1,34 +1,41 @@
 import java.io.File
 import java.net.URL
+import java.util.{List => jList, Map => jMap}
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import fetch.OSGBProduct
 
 import scala.collection.JavaConverters.{mapAsJavaMapConverter, seqAsJavaListConverter}
 
-class AddressLookupFileListFunction extends RequestHandler[String, java.util.List[java.util.Map[String, Object]]] {
-  override def handleRequest(input: String, context: Context): java.util.List[java.util.Map[String, Object]] =
-    {
-      val results = AddressLookup.listAllFileUrlsToDownload()
-        .flatMap { p =>
-          p.zips
-            .map(_.url)
-            .grouped(25)
-            .zipWithIndex
-            .map { case (batch, idx) =>
-              Map(
-                "productName" -> p.productName,
-                "epoch" -> p.epoch.toString,
-                "batchIndex" -> idx.toString,
-                "files" -> batch.filterNot(z =>
-                  new File(s"/mnt/efs/${p.productName}/${p.epoch}/$idx/${fileOf(z)}.done").exists()
-                ).asJava
-              ).asJava
-            }
-        }
+class AddressLookupFileListFunction extends RequestHandler[String, jMap[Int, jList[jMap[String, Object]]]] {
+  override def handleRequest(input: String, context: Context): jMap[Int, jList[jMap[String, Object]]] = {
 
-      println(results.mkString)
-      results.asJava
+    val epochResults = AddressLookup.listAllFileUrlsToDownload()
+      .foldLeft(Map[Int, Seq[OSGBProduct]]()) { case (map, product) =>
+        map ++ Map(product.epoch -> (map.getOrElse(product.epoch, Seq()) :+ product))
+      }
+
+    val results = epochResults.map { case (epoch, products) =>
+      epoch -> products.flatMap { p =>
+        p.zips
+          .map(_.url)
+          .grouped(25)
+          .zipWithIndex
+          .map { case (batch, idx) =>
+            Map(
+              "product" -> p.productName,
+              "batchIndex" -> idx.toString,
+              "files" -> batch.filterNot(z =>
+                new File(s"/mnt/efs/${p.productName}/${p.epoch}/$idx/${fileOf(z)}.done").exists()
+              ).asJava
+            ).asJava
+          }
+      }.asJava
     }
+
+    println(results.mkString)
+    results.asJava
+  }
 
   private def fileOf(url: URL): String = {
     val file = url.getPath
